4394d75319f4f9fb05b14ea3109aaa4781cc3d49
ShuangYa <i@sylingd.com>
2026-03-23 20:19:33 +0800

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export type { EventMap, QQBotConfig } from './client';
-export { default as QQBotClient } from './client';
+export { default } from './client';
 export { ErrorCode, QQBotError } from './core/errorCode';
 export * from './types/index';
